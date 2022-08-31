d428c0cb17925ebbe1aad772050fb0cc1bc5c537
RedHawk <tadshig@gmail.com>
2022-09-01 00:38:27 +0300

    Task3 - kind of done.

--- Lab1/lab1ex3/Debug/src/subdir.mk
+++ Lab1/lab1ex3/Debug/src/subdir.mk
@@ -39,7 +39,7 @@ C_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C++ Compiler'
-	arm-none-eabi-c++ -std=c++11 -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\studies\year3\period1\Embedded_Systems_programming\Labs\Lab1\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\studies\year3\period1\Embedded_Systems_programming\Labs\Lab1\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-c++ -DDEBUG -D__CODE_RED -D__NEWLIB__ -DCORE_M3 -D__USE_LPCOPEN -DCPP_USE_HEAP -D__LPC15XX__ -I"C:\studies\year3\period1\Embedded_Systems_programming\Labs\Lab1\lpc_board_nxp_lpcxpresso_1549\inc" -I"C:\studies\year3\period1\Embedded_Systems_programming\Labs\Lab1\lpc_chip_15xx\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m3 -mthumb -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
