49644465ded62e3276ff8850c7881aa6377bc082
RedHawk <tadshig@gmail.com>
2022-10-05 17:08:28 +0300

    Lab5. Ex1 - done.

--- Lab5/ex1/src/ex1.cpp
+++ Lab5/ex1/src/ex1.cpp
@@ -17,10 +17,27 @@
 #endif
 
 #include <cr_section_macros.h>
+#include "DigitalIoPin.h"
 
-// TODO: insert other include files here
-
-// TODO: insert other definitions and declarations here
+void delayMicroseconds(unsigned int us) 
+{ 
+    // calculate compare value
+    uint64_t stop_time = Chip_Clock_GetSystemClockRate() / 1000000 * us;
+    // disable RIT – compare value may only be changed when RIT is disabled
+    Chip_RIT_Disable(LPC_RITIMER);
+    // set compare value to RIT
+    Chip_RIT_SetCompareValue(LPC_RITIMER, stop_time);
+    // clear RIT counter (so that counting starts from zero)
+    Chip_RIT_SetCounter(LPC_RITIMER, 0);
+    // enable RIT
+    Chip_RIT_Enable(LPC_RITIMER);
+    // wait until RIT Int flag is set
+    while(!Chip_RIT_GetIntStatus(LPC_RITIMER));
+    // disable RIT
+    Chip_RIT_Disable(LPC_RITIMER);
+    // clear RIT Int flag
+    Chip_RIT_ClearIntStatus(LPC_RITIMER);
+} 
 
 int main(void) {
 
@@ -31,21 +48,27 @@ int main(void) {
     // Set up and initialize all required blocks and
     // functions related to the board hardware
     Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
 #endif
 #endif
-
-    // TODO: insert code here
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
+    // Initialise RIT.
+    Chip_RIT_Init(LPC_RITIMER);
+    // Test pin.
+    DigitalIoPin a0(0, 8, false, true, false);
+    a0.write(false);
+    
     while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
+        //• 55 us high
+        a0.write(true);
+        delayMicroseconds(55);
+        //• 35 us low 
+        a0.write(false);
+        delayMicroseconds(35);
+        //• 40 us high
+        a0.write(true);
+        delayMicroseconds(40);
+        //• 20 us low
+        a0.write(false);
+        delayMicroseconds(20);
     }
     return 0 ;
 }
