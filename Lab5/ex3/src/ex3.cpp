59491f8308ab9b658ba2b568ecaa7473ae31a8df
RedHawk <tadshig@gmail.com>
2022-10-06 20:32:54 +0300

    Lab5. Ex3. Done most of the task. Works in an odd way.

--- Lab5/ex3/src/ex3.cpp
+++ Lab5/ex3/src/ex3.cpp
@@ -17,10 +17,46 @@
 #endif
 
 #include <cr_section_macros.h>
+#include "DigitalIoPin.h"
+#include "LpcUart.h"
+#include <atomic>
 
-// TODO: insert other include files here
+static volatile std::atomic_int counter;
+static volatile unsigned int but_presses = 0;
 
-// TODO: insert other definitions and declarations here
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+* @brief Handle interrupt from SysTick timer
+* @return Nothing
+*/
+void SysTick_Handler(void)
+{
+    if(counter > 0) counter--;
+}
+#ifdef __cplusplus
+}
+#endif
+void Sleep(int ms)
+{
+    counter = ms;
+    while(counter > 0) {
+        __WFI();
+    }
+}
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void PIN_INT0_IRQHandler(void) {
+    Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH(0));
+    but_presses++;
+}
+#ifdef __cplusplus
+}
+#endif
 
 int main(void) {
 
@@ -31,21 +67,56 @@ int main(void) {
     // Set up and initialize all required blocks and
     // functions related to the board hardware
     Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
 #endif
 #endif
+    //SysTick
+    uint32_t sysTickRate;
+	Chip_Clock_SetSysTickClockDiv(1);
+	sysTickRate = Chip_Clock_GetSysTickClockRate();
+	SysTick_Config(sysTickRate / 1000);
 
-    // TODO: insert code here
+    //UART setup
+    LpcPinMap none = {-1, -1}; // unused pin has negative values in it
+	LpcPinMap txpin = { 0, 18 }; // transmit pin that goes to debugger's UART->USB converter
+	LpcPinMap rxpin = { 0, 13 }; // receive pin that goes to debugger's UART->USB converter
+	LpcUartConfig cfg = { LPC_USART0, 115200, UART_CFG_DATALEN_8 | UART_CFG_PARITY_NONE | UART_CFG_STOPLEN_1, false, txpin, rxpin, none, none };
+	LpcUart uart(cfg);
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
+    /* Initialize PININT driver */
+	Chip_PININT_Init(LPC_GPIO_PIN_INT);
+
+    //Buttons init.
+    DigitalIoPin sw1(0, 17 ,true ,true, true);
+
+    /* Enable PININT clock */
+	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_PININT);
+
+	/* Reset the PININT block */
+	Chip_SYSCTL_PeriphReset(RESET_PININT);
+
+	/* Configure interrupt channel for the GPIO pin in INMUX block */
+	Chip_INMUX_PinIntSel(0, 0, 17);
+
+	/* Configure channel interrupt as edge sensitive and falling edge interrupt */
+	Chip_PININT_ClearIntStatus(LPC_GPIO_PIN_INT, PININTCH(0));
+	Chip_PININT_SetPinModeEdge(LPC_GPIO_PIN_INT, PININTCH(0));
+	Chip_PININT_EnableIntLow(LPC_GPIO_PIN_INT, PININTCH(0));
+
+	/* Enable interrupt in the NVIC */
+	NVIC_ClearPendingIRQ(PIN_INT0_IRQn);
+	NVIC_EnableIRQ(PIN_INT0_IRQn);
+
+    unsigned int prev_presses = but_presses;
+    unsigned int cur_presses = but_presses;
     while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
+        Sleep(100);
+        cur_presses = but_presses;
+        if(prev_presses != cur_presses) {
+            prev_presses = cur_presses;
+            char buf[36];
+            snprintf(buf, 36, "Button was pressed: %d times\r\n", cur_presses);
+            uart.write(buf);
+        }
     }
     return 0 ;
 }
